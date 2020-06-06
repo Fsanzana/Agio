3d57d5bb5a1f1093869f05635d4a3bcae091ebc9
Felipe Sanzana <f.sanzana01@ufromail.cl>
2020-06-06 16:20:24 -0400

    creacion archivos de datos locales

--- src/main/java/jsoutTesting.java
+++ src/main/java/jsoutTesting.java
@@ -1,10 +1,23 @@
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 
 import java.io.IOException;
 
 public class jsoutTesting {
     public static void main(String[] args) throws IOException {
-        System.out.println( Jsoup.connect("https://preciosmundi.com/chile/precios-supermercado").get());
+        final String url = "https://preciosmundi.com/chile/precios-supermercado";
+
+
+        try{
+            final Document documento = Jsoup.connect(url).get();
+            System.out.println(documento);
+            for(Element row : documento.select("table.tableresponsive tr")){
+
+            }
+        }catch(Exception e){
+            System.out.println(e.getMessage());
+        }
 
     }
 }

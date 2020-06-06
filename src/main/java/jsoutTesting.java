e6dc93d8d62460a291f20a9f3279299529210d44
Felipe Sanzana <f.sanzana01@ufromail.cl>
2020-06-06 17:11:07 -0400

    jsoup funcionando

--- src/main/java/jsoutTesting.java
+++ src/main/java/jsoutTesting.java
@@ -6,16 +6,19 @@ import java.io.IOException;
 
 public class jsoutTesting {
     public static void main(String[] args) throws IOException {
-        final String url = "https://preciosmundi.com/chile/precios-supermercado";
+        final String url = "https://www.numbeo.com/food-prices/country_result.jsp?country=Chile";
 
 
         try{
             final Document documento = Jsoup.connect(url).get();
 
-            for(Element row : documento.select("table.tableresponsive tr")){
-                final String ticker = row.select(".price").text();
-                System.out.println(ticker);
-
+            for(Element row : documento.select("table tr")){
+                if(row.select("td:nth-of-type(2)").text().equals("")){
+                    continue;
+                }else {
+                    final String ticker = row.select("td:nth-of-type(2)").text();
+                    System.out.println(ticker);
+                }
             }
         }catch(Exception e){
             System.out.println(e.getMessage());

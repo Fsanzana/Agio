1fd611ce67af728bac099091a2ab5e34e6644bc0
Vicente Silva <v.silva06@ufromail.cl>
2020-06-06 18:23:27 -0400

    fic

--- src/main/java/jsoutTesting2.java
+++ src/main/java/jsoutTesting2.java
@@ -4,36 +4,31 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 public class jsoutTesting2 {
-    public static void main(String[] args) {
-        final String url =
-                "https://preciosmundi.com/comparacion/argentina_vs_chile/USD";
 
+    public void jsoutTesting2(String url) {
+        final String URL = url;
+        int i = 0;
         try {
-            final Document document = Jsoup.connect(url).get();
+            final Document document = Jsoup.connect(URL).get();
 
-            for (Element row : document.select(
-                    "div.table-responsive tr")) {
+            for (Element row : document.select("div.table-responsive tr")) {
                 if (row.select("td:nth-of-type(1)").text().equals("")) {
                     continue;
-                }
-                else {
-                    final String ticker =
-                            row.select("td:nth-of-type(1)").text();
-                    final String name =
-                            row.select("td.price:nth-of-type(2)").text();
+                } else {
                     final String tempPrice =
                             row.select("td.price:nth-of-type(3)").text();
                     final String tempPrice1 =
-                            tempPrice.replace(",", "");
-//                    final double price = Double.parseDouble(tempPrice1);
+                            tempPrice.replace(",", ".").replace("$", "");
+                    final double price = Double.parseDouble(tempPrice1);
 
-                    System.out.println(ticker + " " + name + " " + tempPrice1);
+                    System.out.println(tempPrice1);
+
                 }
+                i++;
+                System.out.println(i);
             }
-        }
-        catch (Exception ex) {
+        } catch (Exception ex) {
             ex.printStackTrace();
         }
-
     }
 }

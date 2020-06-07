4dfb25cf6e73eb9b7da7a939f0421cd4013a28c0
Felipe Sanzana <f.sanzana01@ufromail.cl>
2020-06-07 18:34:03 -0400

    Validacion menu

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,4 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
@@ -10,39 +11,48 @@ public class Main {
 
 
     private static void menu(Comparador comp) {
+        int eleccion=0;
         String pais0 = "";
         String pais1 = "";
-        System.out.println("1- Chile" + '\n' + "2- Argentina");
-        System.out.println("Ingrese país base:");
-        int eleccion = tec.nextInt();
-        switch (eleccion) {
-            case 1:
-                pais0 = "chile";
-                break;
-            case 2:
-                pais0 = "argentina";
-                break;
-            default:
-                break;
-        }
+        do {
+            try {
+                System.out.println("1- Chile" + '\n' + "2- Argentina");
+                System.out.println("Ingrese país base:");
+                eleccion = tec.nextInt();
+                switch (eleccion) {
+                    case 1:
+                        pais0 = "chile";
+                        System.out.println("Ingrese país a comparar:");
+                        eleccion = tec.nextInt();
+                        break;
+                    case 2:
+                        pais0 = "argentina";
+                        System.out.println("Ingrese país a comparar:");
+                        eleccion = tec.nextInt();
+                        break;
+                    default:
+                        eleccion = 0;
+                        break;
 
-        System.out.println("1- Chile" + '\n' + "2- Argentina");
-
-        System.out.println("Ingrese país a comparar:");
-        eleccion = tec.nextInt();
-        switch (eleccion) {
-            case 1:
-                pais1 = "chile";
-                break;
-            case 2:
-                pais1 = "argentina";
-                break;
-            default:
-                break;
-
-
-        }
-
+                }
+                switch (eleccion) {
+                    case 1:
+                        pais1 = "chile";
+                        break;
+                    case 2:
+                        pais1 = "argentina";
+                        break;
+                    default:
+                        eleccion = 0;
+                        System.out.println("Por favor ingrese una opción valida");
+                        break;
+                }
+            }catch (InputMismatchException e){
+                System.out.println("Por favor ingrese un número entero");
+                tec = new Scanner(System.in);
+                eleccion=0;
+            }
+        }while (eleccion==0);
         System.out.println(comp.porcentaje(pais0, pais1));
 
     }

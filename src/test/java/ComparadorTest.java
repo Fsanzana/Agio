4d668554c242f95040f9cd91471b1f832bd2cad1
Vicente Silva <v.silva06@ufromail.cl>
2020-06-07 17:57:29 -0400

    test clase comparador fix

--- src/test/java/ComparadorTest.java
+++ src/test/java/ComparadorTest.java
@@ -1,5 +1,24 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class ComparadorTest {
 
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void comparador() {
+    }
+
+    @Test
+    public void porcentaje() {
+    }
 }
